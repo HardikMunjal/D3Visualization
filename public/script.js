b955a00b03fa566ca763880ce9c88f40aab67400
hardi <hardik.munjaal@gmail.com>
2016-06-03 12:55:12 +0530

    change styling

--- public/script.js
+++ public/script.js
@@ -1,318 +1,226 @@
 var app = angular.module('visualApp',  []);
 app.controller('visualCtrl', function($scope, $http, $sce) {
 
-    $scope.firstName  = "Hardik";
-    $scope.lastName = "Munjal";
-    var json=[];
-    var admin=[];
-    
-    $http.get("http://localhost:4000/parse/")
-    .then(function(response) {
-
-    $scope.admin =response.data;
-    console.log($scope.admin);
-    });
-
-    $http.get("http://localhost:4000/json/")
-    .then(function(response) {
-
-    var json =response.data;
-    console.log('after http',json[0].id);
-    console.log('after http',json[0].name);
-
-   // var json= JSON.parse(json);
-
-   console.log(typeof(json));
-   console.log($scope.admin);
+  $scope.firstName  = "Hardik";
+  $scope.lastName = "Munjal";
+  $scope.employeeRecord =[];
+  $scope.rootManager=[];
+  $scope.totalEmployees=0;
 
 
-    for(i=0;i<json.length;i++){
-    for(j=0;j<$scope.admin.length;j++){
- 
-         // console.log($scope.admin[j]);
-         //  console.log(json[i]);
 
-        if(JSON.stringify(json[i]) == JSON.stringify($scope.admin[j])){
-            json[i].parent=100;
-            console.log('done');
+//############### Get Root Manager ##############################
+  $http.get("http://localhost:4000/parse1/")
+  .then(function(response) {
+
+    $scope.rootManager =response.data;
+  });
+
+//############### Get All Employee Records #######################
+  $http.get("http://localhost:4000/json1/")
+  .then(function(response) {
+
+   $scope.employeeRecord =response.data[0];
+   $scope.totalEmployees= $scope.employeeRecord.length;
+
+   for(i=0;i<$scope.employeeRecord.length;i++){
+    for(j=0;j<$scope.rootManager.length;j++){
+
+         if(JSON.stringify($scope.employeeRecord[i]) == JSON.stringify($scope.rootManager[j])){
+          $scope.employeeRecord[i].managerId=0;
         }
+      }
     }
-    console.log('atlast you fucker',json[0].name)
-   }
 
-// console.log(xyz[0].id[0].name);
-   var xyz=function getNestedChildren(arr, parent) {
-    var out = []
-    console.log(arr);
-    console.log(parent);
-    for(var i in arr) {
-      console.log('coming here');
-        if(arr[i].parent == parent) {
-          console.log('but not here');
-            var children = getNestedChildren(arr, arr[i].id)
-
-            if(children.length) {
-                arr[i].children = children
-            }
-            out.push(arr[i])
-        }
+//##########  function to convert employee array object to nested array object ################
+var arrayObjectNesting=function getNestedChildren(arr, parent) {
+  var out = []
+  for(var i in arr) {
+    if(arr[i].managerId == parent) {
+      var children = getNestedChildren(arr, arr[i].id)
+      if(children.length) {
+        arr[i].children = children
+      }
+      out.push(arr[i])
     }
-    console.log('aa rha kya bhai tu',out);
-    return out
+  }
+  return out
 }
-var json=xyz(json,100);
-
-console.log(json[0].id)
-
-var xxxy={};
-xxxy.id=1;
-xxxy.children=json;
-
-console.log(json.id);
+$scope.employeeRecord = arrayObjectNesting($scope.employeeRecord,0);
 
 
-    chartNode(xxxy);
-    });
+//###### Converting array_object_result as per the need of d3 #########################
+var nestedObjectForD3={};
+nestedObjectForD3.id=-1;
+nestedObjectForD3.firstName="Employee";
+nestedObjectForD3.children=$scope.employeeRecord;
+
+chartNode(nestedObjectForD3);
+});
 
 
-var chartNode=function maker(json){
 
-var margin = {top: 20, right: 120, bottom: 20, left: 120},
-    width = 560 - margin.right - margin.left,
-    height = 400 - margin.top - margin.bottom;
 
-var i = 0,
+//############ Main Function of D3 which is creating a chart ####################33#
+  var chartNode=function maker(json){
+
+    var margin = {top: 20, right: 120, bottom: 20, left: 120},
+    width = 860 - margin.right - margin.left,
+    height = 500 - margin.top - margin.bottom;
+
+    var i = 0,
     duration = 750,
     root;
 
-var tree = d3.layout.tree()
+    var tree = d3.layout.tree()
     .size([height, width]);
 
-var diagonal = d3.svg.diagonal()
+    var diagonal = d3.svg.diagonal()
     .projection(function(d) { return [d.y, d.x]; });
 
-var svg = d3.select("body").append("svg")
+    var svg = d3.select("body").append("svg")
     .attr("width", width + margin.right + margin.left)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+    .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    root = json;
+    console.log(root.firstName);
+    root.x0 = height / 2;
+    root.y0 = 0;
 
-// var tooltip = d3.select("body")
-//   .append("div")
-//   .style("position", "absolute")
-//   .style("z-index", "10")
-//   .style("visibility", "hidden")
-//   .text("a simple tooltip");
-  
-// var sampleSVG = d3.select(".example_div")
-//   .append("svg:svg")
-//   .attr("class", "sample")
-//   .attr("width", 300)
-//   .attr("height", 300);    
-  
-// d3.select(".example_div svg")
-//   .append("svg:circle")
-//   .attr("stroke", "black")
-//   .attr("fill", "aliceblue")
-//   .attr("r", 50)
-//   .attr("cx", 52)
-//   .attr("cy", 52)
-//   .on("mouseover", function(){return tooltip.style("visibility", "visible");})
-//   .on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
-//   .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
-
-        
-
-// var json ={
-//  "name": "Emploee",
-//  "children": [
-//   {
-//    "name": "Amit",
-//    "children": [
-//     {
-//      "name": "Ankit",
-//      "children": [
-//       {"name": "Hardik", "size": 3938,"children": [
-//                                           {"name": "Naveen", "size": 3938,"children":[
-//                                               {"name": "Naveen", "size": 3938 ,"children":[
-//                                                   {"name": "Naveen", "size": 3938,"children":[
-//                                                   {"name": "Naveen", "size": 3938}
-//                                                   ]}
-//                                                   ]
-//                                                 }]},
-//                                           {"name": "Aanchal", "size": 743} ]
-//   },
-//       {"name": "Atul", "size": 3812},
-//       {"name": "Avinash", "size": 6714},
-//       {"name": "Ashish", "size": 743}
-//      ]
-//     },
-//     {
-//      "name": "Varun",
-//      "children": [
-//       {"name": "Apoorv", "size": 3534},
-//       {"name": "Dinesh", "size": 5731},
-//       {"name": "Benezir", "size": 7840},
-//       {"name": "xxx", "size": 5914}
-//      ]
-//     }
-//    ]
-//   },
-//   {
-//    "name": "Chetan",
-//    "children": [
-//     {
-//      "name": "Faujan",
-//      "children": [
-//       {"name": "ABC", "size": 1302},
-//       {"name": "DEF", "size": 24593},
-//       {"name": "GHI", "size": 652},
-//       {"name": "JKL", "size": 636},
-//       {"name": "ABN", "size": 6703}
-//      ]
-//     },
-//     {
-//      "name": "Sandeep",
-//      "children": [
-//       {"name": "xyz", "size": 2138},
-//       {"name": "x1x", "size": 3824},
-//       {"name": "y1y", "size": 8435}
-//      ]
-//     },
-//     {"name": "Divyesh", "size": 16540}
-//    ]
-//   }
-//  ]
-// };
-
-
-
-
-
-
-
-  root = json;
-  console.log(root.name);
-  root.x0 = height / 2;
-  root.y0 = 0;
-
-  function collapse(d) {
-    if (d.children) {
-      d._children = d.children;
-      d._children.forEach(collapse);
-      d.children = null;
+    function collapse(d) {
+      if (d.children) {
+        d._children = d.children;
+        d._children.forEach(collapse);
+        d.children = null;
+      }
     }
-  }
 
-  root.children.forEach(collapse);
-  update(root);
+    root.children.forEach(collapse);
+    update(root);
 
-d3.select(self.frameElement).style("height", "800px");
+    d3.select(self.frameElement).style("height", "800px");
 
 function update(source) {
 
   // Compute the new tree layout.
   var nodes = tree.nodes(root).reverse(),
-      links = tree.links(nodes);
+  links = tree.links(nodes);
 
   // Normalize for fixed-depth.
   nodes.forEach(function(d) { d.y = d.depth * 100; });
 
   // Update the nodes…
   var node = svg.selectAll("g.node")
-      .data(nodes, function(d) { return d.id || (d.id = ++i); });
+  .data(nodes, function(d) { return d.id || (d.id = ++i); });
 
   // Enter any new nodes at the parent's previous position.
   var nodeEnter = node.enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
-      .on("click", click);
+  .attr("class", "node")
+  .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+  .on("click", click);
 
   nodeEnter.append("circle")
-      .attr("r", 1e-6)
-      .style("fill", function(d) {
-        if(d._children){
-        console.log(d._children.length);
-      }
-      if(d._children && d._children.length>3){
-        console.log('coming here');
-        return d._children ? "red" : "#fff"; 
-      }
-         return d._children ? "lightsteelblue" : "#fff"; 
-       })
-        .append("svg:title").text(function(d) {return "Time: " + d + 
-"\nValue: " + d.name;});
+  .attr("r", 1e-6)
+  .style("fill", function(d) {
+    if(d._children){
+      console.log(d._children.length);
+    }
+    if(d._children && d._children.length>3){
+      console.log('coming here');
+      return d._children ? "red" : "#fff"; 
+    }
+    return d._children ? "lightsteelblue" : "#fff"; 
+  })
+  .append("svg:title").text(function(d) {return "Current Tasks: " + d.isClient + 
+    "\nisBillable: " + d.isBillable;});
 
   nodeEnter.append("text")
-      .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
-      .attr("dy", ".35em")
-      .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
-      .text(function(d) { return d.name; })
-      .style("fill-opacity", 1e-6);
+  .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
+  .attr("dy", ".35em")
+  .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+  .text(function(d) { return d.firstName; })
+  .style("fill-opacity", 1e-6);
 
   // Transition nodes to their new position.
   var nodeUpdate = node.transition()
-      .duration(duration)
-      .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+  .duration(duration)
+  .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
 
   nodeUpdate.select("circle")
-      .attr("r", 4.5)
-      .style("fill", function(d) { 
+  .attr("r", 4)
+  .style("fill", function(d) { 
 
-         if(d._children && d._children.length<=4){
-        console.log('coming here');
-        return "red"; 
-        }
-         if(d._children && d._children.length<4){
-        console.log('coming at yellow');
-        return "yellow"; 
-      }
-      else{
-        return "#fff"}});;
+    $scope.totalEmployees
+
+    if(d._children){
+       var myDirectChildren = d._children.length; 
+       var effectivePercentage = (myDirectChildren/$scope.totalEmployees)*100
+    }
+    
+
+   if(d._children && effectivePercentage>20){
+    console.log('coming here');
+    return "#8DD3C7"; 
+   }
+  if(d._children && effectivePercentage<20 && effectivePercentage>10){
+    console.log('coming here');
+    return "green"; 
+   }
+   if(d._children && effectivePercentage<10 && effectivePercentage>5){
+    console.log('coming here');
+    return "yellow"; 
+   }
+   if(d._children && effectivePercentage<5 && effectivePercentage>0){
+    console.log('coming here');
+    return "#FB8072"; 
+   }
+  else{
+    return "#fff"}});
   
        // return d._children ? "red" : "#fff"; });
 
-  nodeUpdate.select("text")
-      .style("fill-opacity", 1);
+       nodeUpdate.select("text")
+       .style("fill-opacity", 1);
 
   // Transition exiting nodes to the parent's new position.
   var nodeExit = node.exit().transition()
-      .duration(duration)
-      .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
-      .remove();
+  .duration(duration)
+  .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+  .remove();
 
   nodeExit.select("circle")
-      .attr("r", 1e-6);
+  .attr("r", 1e-6);
 
   nodeExit.select("text")
-      .style("fill-opacity", 1e-6);
+  .style("fill-opacity", 1e-6);
 
   // Update the links…
   var link = svg.selectAll("path.link")
-      .data(links, function(d) { return d.target.id; });
+  .data(links, function(d) { return d.target.id; });
 
   // Enter any new links at the parent's previous position.
   link.enter().insert("path", "g")
-      .attr("class", "link")
-      .attr("d", function(d) {
-        var o = {x: source.x0, y: source.y0};
-        return diagonal({source: o, target: o});
-      });
+  .attr("class", "link")
+  .attr("d", function(d) {
+    var o = {x: source.x0, y: source.y0};
+    return diagonal({source: o, target: o});
+  });
 
   // Transition links to their new position.
   link.transition()
-      .duration(duration)
-      .attr("d", diagonal);
+  .duration(duration)
+  .attr("d", diagonal);
 
   // Transition exiting nodes to the parent's new position.
   link.exit().transition()
-      .duration(duration)
-      .attr("d", function(d) {
-        var o = {x: source.x, y: source.y};
-        return diagonal({source: o, target: o});
-      })
-      .remove();
+  .duration(duration)
+  .attr("d", function(d) {
+    var o = {x: source.x, y: source.y};
+    return diagonal({source: o, target: o});
+  })
+  .remove();
 
   // Stash the old positions for transition.
   nodes.forEach(function(d) {
